65ce2c4cc18590b3141e3234bc86038b4f870caf
oladimeji <tommyslipz@gmail.com>
2020-09-15 12:35:00 +0100

    Updates

---
+++ precache-manifest.3d7ae5ebbb5d07fb37bfbba9ce08b384.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf903447857cd396c1980560fad5349",
+    "url": "/shoppie/index.html"
+  },
+  {
+    "revision": "ff24cfbbc3f139ba8731",
+    "url": "/shoppie/static/css/2.19e10c4f.chunk.css"
+  },
+  {
+    "revision": "33db780a97c5a53632d6",
+    "url": "/shoppie/static/css/main.aa89408e.chunk.css"
+  },
+  {
+    "revision": "ff24cfbbc3f139ba8731",
+    "url": "/shoppie/static/js/2.e2b6622a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/shoppie/static/js/2.e2b6622a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33db780a97c5a53632d6",
+    "url": "/shoppie/static/js/main.83d88be9.chunk.js"
+  },
+  {
+    "revision": "d6dc26a0258ab3c4f284",
+    "url": "/shoppie/static/js/runtime-main.46ede51c.js"
+  }
+]);
